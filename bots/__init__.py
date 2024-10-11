66765166bc3e563b0ac07e9cf1fd2c757ea959ca
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-10-11 21:06:03 +0700

    fix(bots): add except for bot template

--- bots/__init__.py
+++ bots/__init__.py
@@ -2,7 +2,9 @@ import importlib
 from glob import glob
 
 for module in glob("bots/*py"):
-    if not module.endswith("__init__.py"):
+    if not module.endswith("__init__.py") and not module.endswith(
+        "custom_bot_template.py"
+    ):
         importlib.import_module(
             module.replace("/", ".").replace("\\", ".").removesuffix(".py")
         )
