14406bf6fe30eb01c305685f115b091a4ca6e8a3
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-10-12 23:36:43 +0700

    feat: add get absolute path

--- bots/__init__.py
+++ bots/__init__.py
@@ -1,10 +1,12 @@
+import os
 import importlib
 from glob import glob
+from lib.utils import resource_path
 
-for module in glob("bots/*py"):
+bot_folder = resource_path("bots")
+for module in glob(os.path.join(bot_folder, "*py")):
     if not module.endswith("__init__.py") and not module.endswith(
         "custom_bot_template.py"
     ):
-        importlib.import_module(
-            module.replace("/", ".").replace("\\", ".").removesuffix(".py")
-        )
+        module_name = os.path.basename(module)[:-3]  # Remove .py extension
+        importlib.import_module(f"bots.{module_name}")
